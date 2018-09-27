8abbc7b3d3c3637e88a20b8a7fc7548f68926f20
Hazel Smith <hazel@hazelesque.uk>
2018-09-27 19:26:40 +0100

    prog 1.2

--- sedgewick-alg-in-c/ch1/src/prog-1.2.c
+++ sedgewick-alg-in-c/ch1/src/prog-1.2.c
@@ -5,12 +5,13 @@
 #define N 10000
 
 /*
- ***********************************************************
- * Program 1.1 Quick-find solution to connectivity problem *
- *                                                         *
- * Source:  Algorithms in C, 3rd Ed., Robert Sedgewick     *
- *          Chapter 1, Section 1.3, Program 1.1. (Page 12) *
- ***********************************************************
+ ***************************************************************
+ * Program 1.2 Quick-union solution to connectivity problem    *
+ *                                                             *
+ * Source(s):   Algorithms in C, 3rd Ed., Robert Sedgewick     *
+ *              Chapter 1, Section 1.3, Program 1.1. (Page 12) *
+ *              Chapter 1, Section 1.3, Program 1.2. (Page 15) *
+ ***************************************************************
  */
 
 main() {
@@ -21,13 +22,10 @@ main() {
         id[i] = i;
 
     while (scanf("%d %d\n", &p, &q) == 2) {
-        if (id[p] == id[q])
-            continue;
-
-        for (t = id[p], i = 0; i < N; i++)
-            if (id[i] == t)
-                id[i] = id[q];
-
+        for (i = p; i != id[i]; i = id[i]) ;
+        for (j = q; j != id[j]; j = id[j]) ;
+        if (i == j) continue;
+        id[i] = j;
         printf(" %d %d\n", p, q);
 
     } // while (scanf(...))
