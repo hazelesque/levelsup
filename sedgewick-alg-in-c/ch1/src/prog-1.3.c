1789a89df76e7a104d9737cfe655c7f2def0b3d4
Hazel Smith <hazel@hazelesque.uk>
2018-09-28 17:39:28 +0100

    Program 1.3: Initial copy from Program 1.2.

---
+++ sedgewick-alg-in-c/ch1/src/prog-1.3.c
@@ -0,0 +1,33 @@
+
+/* vim: set ts=8 sts=4 sw=4 et filetype=c: */
+
+#include <stdio.h>
+#define N 10000
+
+/*
+ ***************************************************************
+ * Program 1.2 Quick-union solution to connectivity problem    *
+ *                                                             *
+ * Source(s):   Algorithms in C, 3rd Ed., Robert Sedgewick     *
+ *              Chapter 1, Section 1.3, Program 1.1. (Page 12) *
+ *              Chapter 1, Section 1.3, Program 1.2. (Page 15) *
+ ***************************************************************
+ */
+
+main() {
+
+    int i, j, p, q, t, id[N];
+
+    for (i = 0; i < N; i++)
+        id[i] = i;
+
+    while (scanf("%d %d\n", &p, &q) == 2) {
+        for (i = p; i != id[i]; i = id[i]) ;
+        for (j = q; j != id[j]; j = id[j]) ;
+        if (i == j) continue;
+        id[i] = j;
+        printf(" %d %d\n", p, q);
+
+    } // while (scanf(...))
+
+} // main()
