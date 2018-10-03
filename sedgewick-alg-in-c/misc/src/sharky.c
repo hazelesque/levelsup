79682c3081c7cb24e4f45248f3debce3b1fb23a4
Hazel Smith <hazel@hazelesque.uk>
2018-10-03 22:43:13 +0100

    sharky: split into two processes with a pipe in between, with naive cat implementation on receiving end for now

--- sedgewick-alg-in-c/misc/src/sharky.c
+++ sedgewick-alg-in-c/misc/src/sharky.c
@@ -1,11 +1,14 @@
 
 /* vim: set ts=8 sts=4 sw=4 et filetype=c: */
 
+#include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-//#include <unistd.h>
-//#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_NAME_LEN 50
 #define MAX_ED_LIMIT 10
@@ -19,12 +22,19 @@
 
 // Usage: $0 [max edit distance] [name] [dictionary file]
 
-void hamming(int max_ed, char *name) {
-    int name_len;
-    char name_temp[MAX_NAME_LEN];
-    int editcols[MAX_ED_LIMIT];
-    int ed, i, j, edit;
-    char c[MAX_ED_LIMIT];
+void hamming(int max_ed, char *name, int fd) {
+    int     name_len;
+    char    name_temp[MAX_NAME_LEN];
+    int     editcols[MAX_ED_LIMIT];
+    int     ed, i, j, edit;
+    char    c[MAX_ED_LIMIT];
+    FILE    *f;
+
+    // Convert fd to FILE* with fdopen
+    if ((f = fdopen(fd, "w")) == NULL) {
+        perror("[hamming] fdopen");
+        exit(4);
+    }
 
     name_len = strlen(name);
 
@@ -83,7 +93,7 @@ void hamming(int max_ed, char *name) {
                     continue;
                 } else if (edit == (ed - 1)) {
                     // No, print candidate
-                    printf("%s\n", name_temp);
+                    fprintf(f, "%s\n", name_temp);
 
                     // Select next set of chars
                     for (j = (ed - 1); j >= 0; ) {
@@ -109,14 +119,51 @@ void hamming(int max_ed, char *name) {
         }
 
     } // for ed
+
+    // Close FILE for pipe fd
+    fclose(f);
+
+}
+
+void catlines(int fd) {
+    char       *buf;
+    size_t      buf_len = ((sizeof(char)) * (MAX_NAME_LEN+1));
+    size_t      chars_read;
+    FILE       *f;
+
+    // Convert fd to FILE* with fdopen
+    if ((f = fdopen(fd, "r")) == NULL) {
+        perror("[catlines] fdopen");
+        exit(4);
+    }
+
+    // Allocate buffer
+    if ((buf = (char *) malloc(buf_len)) == NULL) {
+        perror("malloc");
+        exit(4);
+    }
+
+    // Read lines and re-emit them to stdout
+    //
+    // getline(...) will realloc as necessary if buffer is too small
+    // and hence needs a pointer to our char array pointer so it can
+    // update our pointer for us, and likewise for buf_len.
+    while ((chars_read = getline(&buf, &buf_len, f)) != -1) {
+        fprintf(stdout, "%s", buf);
+    }
+
+    // Clean up
+    fclose(f);
+    if (buf) free(buf);
 }
 
 int main(int argc, char *argv[]) {
-    int max_ed;
-    char *name;
+    int     fd[2], max_ed;
+    char   *name;
+    pid_t   childpid_dictcheck;
+    int     status_dictcheck;
 
     // Check args
-
     if (argc != 4) {
         fprintf(stderr, "Unexpected number of arguments: %d. Exiting.\n", argc - 1);
         return 3;
@@ -125,6 +172,45 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &max_ed);
     name = argv[2];
 
-    hamming(max_ed, name);
+    // Create pipe
+    //
+    if ((pipe(fd)) == -1) {
+        perror("pipe");
+        exit(4);
+    }
+
+    // Fork
+    //
+    if ((childpid_dictcheck = fork()) == -1) {
+        perror("fork");
+        exit(4);
+    }
+
+    if (0 == childpid_dictcheck) {
+        // Child closes input end of pipe
+        close(fd[1]);
+
+        catlines(fd[0]);
+
+        // Tidy up and exit
+        close(fd[0]);
+        exit(0);
+    } else {
+        // Parent closes output end of pipe
+        close(fd[0]);
+
+        hamming(max_ed, name, fd[1]);
+
+        // Tidy up and wait for child to exit
+        close(fd[1]);
+        waitpid(childpid_dictcheck, &status_dictcheck, 0);
+
+        if (status_dictcheck != 0) {
+            fprintf(stderr, "Child %d exited with status %d!\n", childpid_dictcheck, status_dictcheck);
+            exit(5);
+        }
+
+        exit(0);
+    }
 
 } // main()
