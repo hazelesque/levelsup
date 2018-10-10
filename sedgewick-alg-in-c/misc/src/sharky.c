5fe426f0462395a03d5fb6006a73c26c36db3d40
Hazel Smith <hazel@hazelesque.uk>
2018-10-10 23:51:57 +0100

    sharky: part-finished work on reading dictionary, implementing skiplists, adding support for malloc/realloc/free buffer lifecycle to sharkybuf

--- sedgewick-alg-in-c/misc/src/sharky.c
+++ sedgewick-alg-in-c/misc/src/sharky.c
@@ -12,12 +12,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/uio.h>
 
 #define MAX_NAME_LEN 50
 #define MAX_ED_LIMIT 10
+#define SKIPLIST_MAX_LEVELS 30
+#define SKIPLIST_UNROLLED_DATAITEMS 5
+
+#define DEBUG_MSG(format, ...) fprintf(stderr, format, __VA_ARGS__)
 
 /*
  ***************************************************************
@@ -26,12 +31,13 @@
  ***************************************************************
  */
 
-// Usage: $0 [max edit distance] [name] [dictionary file]
+// Usage: $0 <max edit distance> <name> [dictionary file]
 
 
 #define SHARKYBUF_STRATEGY_UNALLOCATED      0
 #define SHARKYBUF_STRATEGY_MMAP             1
 #define SHARKYBUF_STRATEGY_POSIX_MEMALIGN   2
+#define SHARKYBUF_STRATEGY_MALLOC           3
 
 struct sharkybuf {
     /* buffer information */
@@ -47,6 +53,29 @@ struct sharkybuf {
     /* position of writer head */
     char       *writer_ptr;
     size_t      writer_len_remaining;
+};
+
+struct skiplist_node {
+    /* skiplist node with support for multiple links and multiple
+     * data item pointers
+     */
+    int         linkptr_ct;
+    int         dataptr_ct;
+    void       *ptr[]; /* flexible array of type void* */
+};
+
+struct sdict {
+    /* dictonary text */
+    int                     dict_fd;
+    char                   *dict_addr;
+    size_t                  dict_len;
+    /* dictionary index */
+    struct sharkybuf        sl_sbuflist_sbuf;       // Memory where sl_sbuflist is stored
+    int                     sl_sbuflist_entry_ct;   // Number of sbufs in sl_sbuflist
+    struct sharkybuf       *sl_sbuflist;            // List of sbufs, each representing memory
+                                                    //     for storing skiplist nodes in
+    struct skiplist_node   *sl_headnode;            // Pointer to head skiplist node
+    struct skiplist_node   *sl_sentinel;            // Pointer to skiplist sentinel node
 };
 
 void sb_create_mmap(struct sharkybuf *sb, size_t len) {
@@ -124,7 +153,94 @@ void sb_create_posix_memalign(struct sharkybuf *sb, size_t len) {
     sb->writer_len_remaining = len;
 }
 
-void sb_dispose_mmap_(struct sharkybuf *sb) {
+void sb_create_malloc(struct sharkybuf *sb, size_t len) {
+    /*
+     * Create a buffer of size len, allocated by malloc(3).
+     *
+     * Asserts:
+     *      sb is not null
+     *      len > 0
+     */
+    void       *addr;
+
+    // Pre-flight checks
+    assert(sb != NULL);
+    assert(len > 0);
+
+    // Perform allocation
+    addr = malloc(len);
+
+    if (addr == NULL) {
+        perror("[sb_create_malloc] malloc");
+        exit(4);
+    }
+
+    // Zero buffer
+    memset(addr, 0, len);
+
+    // Populate struct
+    sb->strategy = SHARKYBUF_STRATEGY_MALLOC;
+    sb->addr = addr;
+    sb->len = len;
+    sb->dirty = false;
+
+    // Initialize "writer head" position
+    sb->writer_ptr = (char*)addr;
+    sb->writer_len_remaining = len;
+}
+
+void sb_realloc(struct sharkybuf *sb, size_t new_len) {
+    /*
+     * Realloc(3) a buffer previously allocated by malloc(3),
+     * to have a new size of new_len
+     *
+     * Asserts:
+     *      sb is not null
+     *      sb->strategy is SHARKYBUF_STRATEGY_MALLOC
+     *      new_len > 0
+     *      new_len > sb->len
+     */
+    void       *new_addr;
+    void       *old_addr;
+    size_t      old_len;
+    ptrdiff_t   addr_delta;
+    size_t      len_delta;
+
+    // Pre-flight checks
+    assert(sb != NULL);
+    assert(sb->strategy == SHARKYBUF_STRATEGY_MALLOC);
+    assert(new_len > 0);
+    assert(new_len > sb->len);
+
+    // Prep
+    old_addr = sb->addr;
+    old_len = sb->len;
+    len_delta = new_len - old_len;
+
+    // Perform allocation
+    new_addr = realloc(old_addr, new_len);
+
+    if (new_addr == NULL) {
+        perror("[sb_realloc] realloc");
+        exit(4);
+    }
+
+    // Zero new part of buffer
+    memset((new_addr + old_len), 0, (new_len - old_len));
+
+    // Calculate difference in addresses
+    addr_delta = new_addr - old_addr;
+
+    // Update struct
+    sb->addr = new_addr;
+    sb->len = new_len;
+
+    // Update "writer head"
+    sb->writer_ptr += addr_delta;
+    sb->writer_len_remaining += len_delta;
+}
+
+void sb_dispose_munmap_(struct sharkybuf *sb) {
     /*
      * Dispose of buffer we allocated previously with mmap(2)
      *
@@ -149,20 +265,23 @@ void sb_dispose_mmap_(struct sharkybuf *sb) {
     sb->writer_len_remaining = 0;
 }
 
-void sb_dispose_posix_memalign_(struct sharkybuf *sb) {
+void sb_dispose_free_(struct sharkybuf *sb) {
     /*
-     * Dispose of buffer we allocated previously with posix_memalign(3)
+     * Dispose of buffer we allocated previously with either
+     * posix_memalign(3) or malloc(3)
      *
      * Asserts:
      *      sb is not NULL
      *      sb->addr is not NULL
      *      sb->strategy is SHARKYBUF_STRATEGY_POSIX_MEMALIGN
+     *                   or SHARKYBUF_STRATEGY_MALLOC
      */
 
     // Pre-flight checks
     assert(sb != NULL);
     assert(sb->addr != NULL);
-    assert(sb->strategy == SHARKYBUF_STRATEGY_POSIX_MEMALIGN);
+    assert((sb->strategy == SHARKYBUF_STRATEGY_POSIX_MEMALIGN) ||
+           (sb->strategy == SHARKYBUF_STRATEGY_MALLOC));
 
     // Actually free the page(s)
     free(sb->addr);
@@ -179,10 +298,11 @@ void sb_dispose_posix_memalign_(struct sharkybuf *sb) {
 void sb_dispose(struct sharkybuf *sb) {
     switch (sb->strategy) {
         case SHARKYBUF_STRATEGY_MMAP:
-            sb_dispose_mmap_(sb);
+            sb_dispose_munmap_(sb);
             break;
         case SHARKYBUF_STRATEGY_POSIX_MEMALIGN:
-            sb_dispose_posix_memalign_(sb);
+        case SHARKYBUF_STRATEGY_MALLOC:
+            sb_dispose_free_(sb);
             break;
         default:
             fprintf(stderr, "[sb_dispose] invalid strategy %d.\n", sb->strategy);
@@ -600,20 +720,297 @@ void catlines(int fd) {
     sb_dispose(&sbuf);
 }
 
+void sdict_sl_realloc(struct sdict *sd) {
+    size_t          new_len;
+
+    // Calculate new size
+    new_len = sd->sl_sbuflist_sbuf.len * 2;
+
+    // Request realloc
+    sb_realloc(&(sd->sl_sbuflist_sbuf), new_len);
+
+    // Update address of sd->sl_sbuflist
+    sd->sl_sbuflist = sd->sl_sbuflist_sbuf.addr;
+}
+
+struct skiplist_node* sdict_sl_allocnode(struct sdict *sd, int linkptr_ct, int dataptr_ct) {
+    size_t                  page_size;
+    struct skiplist_node   *node_addr;
+    size_t                  node_size;
+
+    page_size = (size_t)sysconf(_SC_PAGESIZE);
+
+    // Calculate size required
+    node_size = sizeof(struct skiplist_node) + ((linkptr_ct + dataptr_ct) * sizeof(void*));
+
+    // Check available space
+    if (((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]).writer_len_remaining < node_size) {
+        // Need another buffer...
+        //
+        // First, check if memory allocation for list of buffers needs enlarging...
+        if (sd->sl_sbuflist_sbuf.writer_len_remaining < sizeof(struct sharkybuf)) {
+            sdict_sl_realloc(sd);
+        }
+
+        // Assert that there is enough room now for another sharkybuf struct
+        assert(sd->sl_sbuflist_sbuf.writer_len_remaining >= sizeof(struct sharkybuf));
+
+        // Create new buffer
+        page_size = (size_t)sysconf(_SC_PAGESIZE);
+        sb_create_malloc(&((sd->sl_sbuflist)[sd->sl_sbuflist_entry_ct]), page_size);
+        (sd->sl_sbuflist_entry_ct)++;
+    }
+
+    // Assert that there is enough room now for the new node
+    assert(((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]).writer_len_remaining >= node_size);
+
+    // Initialize skiplist node
+    node_addr = (struct skiplist_node*)(((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]).writer_ptr);
+    node_addr->linkptr_ct = linkptr_ct;
+    node_addr->dataptr_ct = dataptr_ct;
+
+    // Bump writer pointer
+    ((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]).writer_ptr += (node_size / sizeof(char));
+    ((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]).writer_len_remaining -= node_size;
+
+    return node_addr;
+}
+
+void sdict_sl_init(struct sdict *sd) {
+    /*
+     * Initialize skiplist datastructure, allocating required buffers and
+     * generating the head and sentinel nodes of the skiplist.
+     *
+     * Asserts:
+     *          sd is not NULL
+     */
+    size_t              page_size;
+
+    // Pre-flight checks
+    assert(sd != NULL);
+
+    // Allocate buffer to store skiplist buffer list in
+    //
+    // N.B. This buffer can be moved safely (e.g. when resized with
+    // realloc), as there is only ever one pointer to it, and it is
+    // only used to keep track of freeing buffers from the pool used
+    // for the skiplist index.
+
+    page_size = (size_t)sysconf(_SC_PAGESIZE);
+    sb_create_malloc(&(sd->sl_sbuflist_sbuf), page_size);
+
+    DEBUG_MSG("-DD- Allocated sl_sbuflist_sbuf, .addr=%p, .len=%d.\n", sd->sl_sbuflist_sbuf.addr, sd->sl_sbuflist_sbuf.len);
+
+    sd->sl_sbuflist = (struct sharkybuf*)(sd->sl_sbuflist_sbuf.addr);
+    sd->sl_sbuflist_entry_ct = 0;
+
+    // Allocate buffer to store skiplist index in
+    sb_create_malloc(&((sd->sl_sbuflist)[sd->sl_sbuflist_entry_ct]), page_size);
+    (sd->sl_sbuflist_entry_ct)++;
+
+    DEBUG_MSG("-DD- Allocated sl_sbuflist[%d], .addr=%p, .len=%d. sl_sbuflist_entry_ct is now %d.\n", (sd->sl_sbuflist_entry_ct - 1), (sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)].addr, (sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)].len, sd->sl_sbuflist_entry_ct);
+
+    // Bump sd->sl_sbuflist_sbuf writer pointer
+    sd->sl_sbuflist_sbuf.writer_ptr += (sizeof(struct sharkybuf) / sizeof(char));
+    sd->sl_sbuflist_sbuf.writer_len_remaining -= sizeof(struct sharkybuf);
+
+    // Initialize skiplist headnode
+    sd->sl_headnode = sdict_sl_allocnode(sd, SKIPLIST_MAX_LEVELS, 0);
+
+    DEBUG_MSG("-DD- Allocated sl_headnode at address %p.\n", sd->sl_headnode);
+
+    // Initialise skiplist sentinel node
+    sd->sl_sentinel = sdict_sl_allocnode(sd, 0, 0);
+
+    DEBUG_MSG("-DD- Allocated sl_sentinel at address %p.\n", sd->sl_sentinel);
+
+    // Initialize skiplist headnode pointers
+    for (int i = 0; i < (sd->sl_headnode->linkptr_ct + sd->sl_headnode->dataptr_ct); i++) {
+        sd->sl_headnode->ptr[i] = sd->sl_sentinel;
+    }
+}
+
+void sdict_sl_destruct(struct sdict *sd) {
+    /*
+     * Free all buffers allocated for the skiplist, and clear
+     * skiplist-related struct members
+     *
+     * Asserts:
+     *          sd is not NULL
+     */
+
+    // Pre-flight checks
+    assert(sd != NULL);
+
+    // Dispose of all sbufs in the pool
+    while(sd->sl_sbuflist_entry_ct > 0) {
+        sb_dispose(&((sd->sl_sbuflist)[(sd->sl_sbuflist_entry_ct - 1)]));
+        (sd->sl_sbuflist_entry_ct)--;
+    }
+
+    // Dispose of sbuf used to store pool sbuf structs
+    sb_dispose(&(sd->sl_sbuflist_sbuf));
+
+    // Clear skiplist struct entries
+    sd->sl_sentinel = NULL;
+    sd->sl_headnode = NULL;
+    sd->sl_sbuflist = NULL;
+}
+
+void sdict_open(struct sdict *sd, char *dictpath) {
+    /*
+     * Open dictionary at dictpath, mmap it, process it into a skiplist
+     * data structure and store necessary information to access it in *sb.
+     *
+     * Asserts:
+     *          sd is not NULL
+     *          dictpath is not NULL
+     */
+    char               *dict_addr;
+    int                 dict_fd;
+    int                 fst_rv;
+    struct stat         dict_statbuf;
+    size_t              dict_len;
+
+    // Pre-flight checks
+    assert(sd != NULL);
+    assert(dictpath != NULL);
+
+    // Open
+    dict_fd = open(dictpath, O_RDONLY);
+
+    if (dict_fd == -1) {
+        perror("[sdict_open] open");
+        exit(4);
+    }
+
+    // Get size
+    fst_rv = fstat(dict_fd, &dict_statbuf);
+
+    if (fst_rv == -1) {
+        perror("[sdict_open] fstat");
+        exit(4);
+    }
+
+    dict_len = dict_statbuf.st_size;
+
+    // Mmap
+    dict_addr = mmap(NULL, dict_len, PROT_READ, MAP_PRIVATE, dict_fd, 0);
+
+    if (dict_addr == MAP_FAILED) {
+        perror("[sdict_open] mmap");
+        exit(4);
+    }
+
+    // Populate struct
+    sd->dict_addr = dict_addr;
+    sd->dict_fd = dict_fd;
+    sd->dict_len = dict_len;
+
+    // Initialize skiplist
+    sdict_sl_init(sd);
+
+    // Populate skiplist from dictionary
+
+}
+
+void sdict_close(struct sdict *sd) {
+    int         munmap_rv;
+    int         close_rv;
+
+    // Free buffers used by skiplist and by buffer pool
+    sdict_sl_destruct(sd);
+
+    // Munmap
+    munmap_rv = munmap(sd->dict_addr, sd->dict_len);
+
+    if (munmap_rv == -1) {
+        perror("[sdict_close] munmap");
+        exit(4);
+    }
+
+    // Close file
+    close_rv = close(sd->dict_fd);
+
+    if (close_rv == -1) {
+        perror("[sdict_close] close");
+        exit(4);
+    }
+
+    // Clear struct
+    sd->dict_fd = 0;
+    sd->dict_addr = NULL;
+    sd->dict_len = 0;
+}
+
+void checkwords(int fd, char *dictpath) {
+    /*
+     * Read buffer-sized chunks from pipe fd containing zero or more newline-separated
+     * candidate words followed by null bytes up to the end of the buffer, and write
+     * those that appear in dictionary file dictpath to standard output.
+     */
+    struct sharkybuf    candw_sbuf;
+    size_t              candw_buf_len;
+    struct sdict        sd;
+    size_t              page_size;
+    int                 read_rv;
+
+    // Get system page size
+    page_size = (size_t)sysconf(_SC_PAGESIZE);
+
+    // Read in dictionary
+    sdict_open(&sd, dictpath);
+
+    // Allocate buffer to receive candidate words
+    candw_buf_len = page_size;
+    sb_create_posix_memalign(&candw_sbuf, candw_buf_len);
+
+    // Read buffer-size chunks of candidate words from fd, and check against dictionary
+    while (true) {
+        read_rv = sb_recvbuf_read(&candw_sbuf, fd);
+
+        // Check words and emit those that appear in the dictionary to standard output
+        //XXXX;
+
+        // Wipe buffer and reset writer head
+        sb_wipe(&candw_sbuf);
+
+        // Did we reach EOF?
+        if (read_rv == 1) break;
+    }
+
+    // Close dictionary
+    sdict_close(&sd);
+
+    // Clean up
+    sb_dispose(&candw_sbuf);
+}
+
+void usage(char *progname) {
+    fprintf(stderr, "Usage: %s <max hamming distance> <name> [dictionary file]\n", progname);
+}
+
 int main(int argc, char *argv[]) {
     int     fd[2], max_ed;
+    char   *dictpath;
     char   *name;
     pid_t   childpid_dictcheck;
     int     status_dictcheck;
 
-    // Check args
-    if (argc != 4) {
-        fprintf(stderr, "Unexpected number of arguments: %d. Exiting.\n", argc - 1);
-        return 3;
+    // Check and extract command-line arguments
+    switch (argc) {
+        case 4:
+            dictpath = argv[3];
+        case 3:
+            sscanf(argv[1], "%d", &max_ed);
+            name = argv[2];
+            break;
+        default:
+            fprintf(stderr, "%s: Unexpected number of arguments: %d. Exiting.\n\n", argv[0], argc - 1);
+            usage(argv[0]);
+            return 3;
     }
 
-    sscanf(argv[1], "%d", &max_ed);
-    name = argv[2];
 
     // Create pipe
     //
@@ -633,7 +1030,11 @@ int main(int argc, char *argv[]) {
         // Child closes input end of pipe
         close(fd[1]);
 
-        catlines(fd[0]);
+        if (dictpath) {
+            checkwords(fd[0], dictpath);
+        } else {
+            catlines(fd[0]);
+        }
 
         // Tidy up and exit
         close(fd[0]);
